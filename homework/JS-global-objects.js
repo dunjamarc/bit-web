a439e2b8f971b712fff2ccd89b118fda0cad166b
dunjamarc <dunjamarkovic86@gmail.com>
2018-02-27 23:54:57 +0100

    math object

--- homework/JS-global-objects.js
+++ homework/JS-global-objects.js
@@ -1,12 +1,44 @@
-function random(){
+var myArr = (function(){
     var arr = [];
     for(var i = 0; i < 10; i++){
-        arr.push(Math.floor(((50 - 1) * Math.random()) + 1));
+        arr.push(((50 - 1) * Math.random()) + 1);
     }
-    console.log(arr);
+    return arr;
+})()
+
+console.log(myArr);
+
+
+//--------------------------
+
+function round(arr){
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++){
+        newArr.push(Math.round(arr[i]).toFixed(2));
+    }
+    return newArr;
 }
+console.log(round(myArr));
 
-random();
+//--------------------------
 
-//--------------------
+function floor(arr){
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++){
+        newArr.push(Math.floor(arr[i]));
+    }
+    return newArr;
+}
+console.log(floor(myArr));
+
+//---------------------------------
+
+function maxNum(arr){
+    return Math.max(...arr);  // ... max with array
+}
+console.log(maxNum(myArr));
+
+//-------------------------------DATE
+
+console.log(Date());
 
