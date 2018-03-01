7c49c34b1e797bf345a252c60d8f02c2b15f5a3c
BIT Student <classroom@bgit.rs>
2018-03-01 18:15:36 +0100

    super mario running

---
+++ Super Mario/main.js
@@ -0,0 +1,33 @@
+var backgroundPosition = 0;
+var timer;
+
+function runMario() {
+    document.querySelector('#run').setAttribute('style', 'display: block');
+    document.querySelector('#stand').setAttribute('style', 'display: none');
+
+    timer = setInterval(function() {
+        backgroundPosition -= 10;
+        document.querySelector('body').setAttribute('style', 'background-position-x:' + backgroundPosition + 'px');
+    }, 50);
+    console.log(backgroundPosition);
+}
+
+function stopMario() {
+    document.querySelector('#stand').setAttribute('style', 'display: block');
+    document.querySelector('#run').setAttribute('style', 'display: none');
+    clearInterval(timer);  // doesn't stop
+}
+
+
+document.querySelector('body').addEventListener('keydown', function (event) {
+    if (event.keyCode === 39) {
+        runMario();
+    }
+});
+
+document.querySelector('body').addEventListener('keyup', function (event) {
+    if (event.keyCode === 39) {
+        stopMario();
+    }
+}
+);
